87db9754ad9d6873f7e23062c9c6b9c4fe153961
root <aadlg9396@gmail.com>
2017-11-08 21:35:47 +0700

    from 8.11

---
+++ example06.js
@@ -0,0 +1,55 @@
+var http = require("http").createServer(handler);
+var io = require("socket.io").listen(http);
+var fs=require("fs")
+var firmata = require("firmata");
+
+var board = new firmata.Board("/dev/ttyACM0", function(){ // ACM Abstract Control Model for serial communication with Arduino (could be USB)
+    console.log("Connecting to Arduino");
+    console.log("Activation of Pin 8");
+    console.log("Enabling Push Button on pin 2");
+board.pinMode(2, board.MODES.INPUT);
+
+});
+
+function handler(req,res) {
+    fs.readFile(__dirname + "/example05.html",
+    function (err, data) {
+        if (err) {
+            res.writeHead(500, {'Content-Type': "text/plain"});
+            return res.end("Error loading html page.");
+         }
+        res.writeHead(200);
+        res.end(data);
+        });
+}
+
+http.listen(8080);
+
+io.sockets.on("connection", function(socket) {
+socket.on("commandToArduino", function(commandNo){
+        if (commandNo == 0) {
+            board.digitalWrite(13, board.LOW); // write LOW on pin 13
+        }
+        if (commandNo == "1") {
+            board.digitalWrite(13, board.HIGH); // write HIGH on pin 13
+        }
+        if (commandNo == "2") {
+            board.digitalWrite(8, board.LOW); // write LOW on pin 8
+        }
+        if (commandNo == "3") {
+            board.digitalWrite(8, board.HIGH); // write HIGH on pin 8
+        } 
+         if (commandNo == "4") {
+            board.digitalWrite(3, board.LOW); // write LOW on pin 13
+        }
+        if (commandNo == "5") {
+            board.digitalWrite(3, board.HIGH); // write HIGH on pin 13
+        }
+         if (commandNo == "6") {
+            board.digitalWrite(12, board.LOW); // write LOW on pin 13
+        }
+        if (commandNo == "7") {
+            board.digitalWrite(12, board.HIGH); // write HIGH on pin 13
+        }
+    });
+});
